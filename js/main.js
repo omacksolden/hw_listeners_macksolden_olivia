a388404b29da103c86da6c1c34f7d8a9e492bbb9
Olivia Mack-Solden <omacksolden@gmail.com>
2016-07-14 22:15:00 -0400

    Simplify event listener code

--- js/main.js
+++ js/main.js
@@ -2,17 +2,8 @@ var heading = document.getElementById('heading');
 
 heading.addEventListener('click', function () {
     var myElement = document.createElement('article');
-    myElement.innerHTML = '<p>This is click number x</p>';
+    var x = 1;
+    myElement.innerHTML = 'This is click number ' + x ;
+    x++;
     document.getElementsByClassName('container')[0].appendChild(myElement);
 });
-
-//
-// function modifyHTML() {
-//  var heading = document.getElementById( 'heading' );
-//  heading.innerHTML = 'This is click number x!';
-// }
-//
-// search addEventListener("click", function modifyHTML() {
-// 	var heading = document.getElementById( 'firstHeading' );
-// 	heading.innerHTML = 'This is click number x!';
-// });
