851fb8de0f832b3795f8e22a29da73b3461e21d5
Olivia Mack-Solden <omacksolden@gmail.com>
2016-07-15 07:26:53 -0400

    Increment variable and add cursor change

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var heading = document.getElementById('heading');
-
+var x = 1;
 heading.addEventListener('click', function () {
     var myElement = document.createElement('article');
-    var x = 1;
-    myElement.innerHTML = 'This is click number ' + x ;
+    myElement.innerHTML = 'This is click number ' + x;
     x++;
     document.getElementsByClassName('container')[0].appendChild(myElement);
 });
