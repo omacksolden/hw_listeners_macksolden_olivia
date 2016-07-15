0ca2fa0efc5ad853f6792efb07742495eb0aca67
Olivia Mack-Solden <omacksolden@gmail.com>
2016-07-15 06:55:20 -0400

    Add listener to image to invert on double click

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ var heading = document.getElementById('heading');
 heading.addEventListener('click', function () {
     heading.className = 'teal';
 });
+
+var theImage = document.images[0];
+
+theImage.addEventListener('dblclick', function () {
+    theImage.className = 'invert';
+});
+
+
+theImage.removeEventListener('dblclick', function () {
+    theImage.className = 'invert';
+});
